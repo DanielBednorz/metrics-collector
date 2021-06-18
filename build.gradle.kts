153bbfb67033a73cde001335b56210f117ad5fcf
LAPTOP-43FL476R\Daniel <daniel.bednorz@keycon.de>
2021-06-18 06:27:37 +0200

    Add mockk

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("io.mockk:mockk:1.11.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.cloud:spring-cloud-contract-wiremock:3.0.1")
 }
